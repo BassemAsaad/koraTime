ed676d161b95749c172dbfd976cb1b0d4b8ea372
Bassem <bassemasaad11@gmail.com>
2024-04-14 04:40:15 +0200

    change text in roomImageText_layout

--- app/src/main/java/com/example/koratime/createRoom/AddRoomActivity.kt
+++ app/src/main/java/com/example/koratime/createRoom/AddRoomActivity.kt
@@ -1,10 +1,12 @@
 package com.example.koratime.createRoom
 
+import android.annotation.SuppressLint
 import android.content.Intent
 import android.os.Bundle
 import android.provider.MediaStore
 import android.util.Log
 import android.widget.Toast
+import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.PickVisualMediaRequest
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.lifecycle.ViewModelProvider
@@ -13,9 +15,11 @@ import com.example.koratime.rooms.RoomsFragment
 import com.example.koratime.basic.BasicActivity
 import com.example.koratime.databinding.ActivityAddRoomBinding
 
-
 @Suppress("DEPRECATION")
 class AddRoomActivity : BasicActivity< ActivityAddRoomBinding,AddRoomViewModel>(),AddRoomNavigator{
+
+    lateinit var pickMedia : ActivityResultLauncher<PickVisualMediaRequest>
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         initView()
@@ -29,6 +33,7 @@ class AddRoomActivity : BasicActivity< ActivityAddRoomBinding,AddRoomViewModel>(
         return ViewModelProvider(this)[AddRoomViewModel::class.java]
     }
 
+    @SuppressLint("SetTextI18n")
     override fun initView() {
         viewModel.navigator = this
         dataBinding.vm = viewModel
@@ -40,30 +45,31 @@ class AddRoomActivity : BasicActivity< ActivityAddRoomBinding,AddRoomViewModel>(
         supportActionBar?.setDisplayShowHomeEnabled(true)
 
         openImagePicker()
-
+        dataBinding.roomImageLayout.setOnClickListener {
+            // Launch the photo picker and let the user choose only images.
+            pickMedia.launch(PickVisualMediaRequest(ActivityResultContracts.PickVisualMedia.ImageOnly))
+        }
 
     }
-
     fun openImagePicker(){
         // Registers a photo picker activity launcher in single-select mode.
-        val pickMedia = registerForActivityResult(ActivityResultContracts.PickVisualMedia()) { uri ->
+         pickMedia = registerForActivityResult(ActivityResultContracts.PickVisualMedia()) { uri ->
             // Callback is invoked after the user selects a media item or closes the
             // photo picker.
             if (uri != null) {
                 Log.d("PhotoPicker", "Selected URI: $uri")
+                dataBinding.roomImageLayout.setImageURI(uri)
+                dataBinding.roomImageTextLayout.text = "Change Picture Chosen"
             } else {
                 Log.d("PhotoPicker", "No media selected")
             }
         }
 
-        dataBinding.roomImageLayout.setOnClickListener {
-            // Launch the photo picker and let the user choose only images.
-            pickMedia.launch(PickVisualMediaRequest(ActivityResultContracts.PickVisualMedia.ImageOnly))
-        }
+
     }
 
     override fun onSupportNavigateUp(): Boolean {
-        // Navigate back to the previous screen when the back button is clicked on the toolbar
+        // go back to the previous fragment when back button clicked on the toolbar
         onBackPressed()
         return true
     }
--- app/src/main/java/com/example/koratime/stadiums/StadiumsFragment.kt
+++ app/src/main/java/com/example/koratime/stadiums/StadiumsFragment.kt
@@ -1,9 +1,13 @@
 package com.example.koratime.stadiums
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.PickVisualMediaRequest
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
@@ -11,7 +15,7 @@ import com.example.koratime.R
 import com.example.koratime.databinding.FragmentStadiumsBinding
 
 class StadiumsFragment : Fragment(),StadiumsNavigator{
-
+    lateinit var pickMedia : ActivityResultLauncher<PickVisualMediaRequest>
     lateinit var dataBinding : FragmentStadiumsBinding
     lateinit var viewModel : StadiumsViewModel
 
@@ -41,5 +45,28 @@ class StadiumsFragment : Fragment(),StadiumsNavigator{
     fun initView() {
         dataBinding.vm = viewModel
         viewModel.navigator=this
+
+
+        openImagePicker()
+        //data binding
+            // Launch the photo picker and let the user choose only images.
+            pickMedia.launch(PickVisualMediaRequest(ActivityResultContracts.PickVisualMedia.ImageOnly))
+
+
+    }
+    fun openImagePicker(){
+        // Registers a photo picker activity launcher in single-select mode.
+        pickMedia = registerForActivityResult(ActivityResultContracts.PickVisualMedia()) { uri ->
+            // Callback is invoked after the user selects a media item or closes the
+            // photo picker.
+            if (uri != null) {
+                Log.d("PhotoPicker", "Selected URI: $uri")
+//                dataBinding.roomImageLayout.setImageURI(uri)
+            } else {
+                Log.d("PhotoPicker", "No media selected")
+            }
+        }
+
+
     }
 }
