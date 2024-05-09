3d26c0fb44383ea55c4bacb912cd9aca07a09a1a
Bassem <bassemasaad11@gmail.com>
2024-05-09 16:04:13 +0300

    finish graduation project

--- app/src/main/java/com/example/koratime/location/LocationPickerViewModel.kt
+++ app/src/main/java/com/example/koratime/location/LocationPickerViewModel.kt
@@ -4,6 +4,4 @@ package com.example.koratime.location
 import com.example.koratime.basic.BasicViewModel
 import org.checkerframework.checker.units.qual.N
 class LocationPickerViewModel : BasicViewModel<LocationPickerNavigator>() {
-
-
 }
